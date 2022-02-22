9b70e6149ddf4f919af362e37fa92804ac66a49c
Sylvie-B <unsolo@hotmail.fr>
2022-02-22 16:34:54 +0100

    agregation

--- index.php
+++ index.php
@@ -9,11 +9,13 @@
 </head>
 <body>
     <?php
+
     /**
      * 1. Importez le contenu du fichier user.sql dans une nouvelle base de données.
      * 2. Utilisez un des objets de connexion que nous avons fait ensemble pour vous connecter à votre base de données.
      *
-     * Pour chaque résultat de requête, affichez les informations, ex:  Age minimum: 36 ans <br>   ( pour obtenir une information par ligne ).
+     * Pour chaque résultat de requête, affichez les informations, ex:  Age minimum: 36 ans <br>
+     *   ( pour obtenir une information par ligne ).
      *
      * 3. Récupérez l'age minimum des utilisateurs.
      * 4. Récupérez l'âge maximum des utilisateurs.
@@ -24,7 +26,45 @@
      */
 
     // TODO Votre code ici, commencez par require un des objet de connexion que nous avons fait ensemble.
+    require 'connPDO.php';
+    $pdo = new connPDO();
+    $db = $pdo->conn();
 
+    $stm = $db->prepare("SELECT MIN(age) as minimum FROM user");
+    $stm->execute();
+    $minAge = $stm->fetch();
+    echo "age minimum : " . $minAge["minimum"] . "<br>";
+
+    $stm = $db->prepare("SELECT MAX(age) as max FROM user");
+    $stm->execute();
+    $minAge = $stm->fetch();
+    echo "age maximum : " . $minAge["max"] . "<br>";
+
+    $stm = $db->prepare("SELECT count(*) as number FROM user");
+    $stm->execute();
+    if($stm->execute()){
+        $count = $stm->fetch();
+        echo "il y a " . $count['number'] . " d'utilisateurs" . "<br>";
+    }
+
+    $stm = $db->prepare("SELECT count(*) as number FROM user WHERE numero >= 5");
+    $stm->execute();
+    if($stm->execute()){
+        $count = $stm->fetch();
+        echo "il y a " . $count['number'] . " numéro de rue > ou = 5" . "<br>";
+    }
+
+    $stm = $db->prepare("SELECT AVG(age) as moy FROM user");
+    if($stm->execute()){
+        $nbr = $stm->fetch();
+        echo "moyenne d'age : " . $nbr['moy'] . "<br>";
+    }
+
+    $stm = $db->prepare("SELECT SUM(numero) as numero FROM user");
+    if($stm->execute()){
+        $nbr = $stm->fetch();
+        echo "somme des numéros : " . $nbr['numero'] . "<br>";
+    }
 
     ?>
 </body>
